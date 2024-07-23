c01e3ef3e949bcacdfd9e9a2a7429fffd9c8007d
Vasilis <tomaras.vasi@outlook.com>
2024-07-23 14:13:04 +0200

    stationwidget meldingen en afstand

--- src/Components/NSWidget.jsx
+++ src/Components/NSWidget.jsx
@@ -3,10 +3,21 @@ import { getNearByStations } from "../API-requests/NS-API";
 import StationWidget from "./StationWidget";
 import NsLogo from './../../public/assets/Images/NS-Logo.png';
 import './NS.css';
+import { Carousel } from "react-responsive-carousel";
+import Slider from "react-slick";
 
 const NSWidget = () => {
     const [stations, setStations] = useState([]);
     const [error, setError] = useState(null);
+
+    const settings = {
+        dots: false,
+        Infinite: true,
+        arrows: false,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed: 10000
+    }
 
     useEffect(() => {
         const fetchStations = async () => {
@@ -22,29 +33,8 @@ const NSWidget = () => {
     }, []);
 
     // Commented out code for rendering multiple stations
-    // const renderStations = () => (
-    //     stations.map(station => (
-    //         <div className="widget-ut" key={station.UICCode}>
-    //             <p className="stationHeader">
-    //                 <img src={NsLogo} className="ns-logo" alt="NS Logo" />
-    //                 {station.namen.lang}
-    //             </p>
-    //             <div className="NSspacing bg-yellow-dark">
-    //                 <p>Tijd</p>
-    //                 <p>Bestemming</p>
-    //                 <p>Trein</p>
-    //                 <p className="platformHeader">Perron</p>
-    //             </div>
-    //             <StationWidget stationID={station.UICCode} />
-    //         </div>
-    //     ))
-    // );
-
-    const renderSingleStation = () => {
-        if (stations.length === 0) return <p>No stations available</p>;
-        const station = stations.find(station => station.namen.lang === 'Utrecht CS') || stations[1];
-
-        return (
+    const renderStations = 
+        stations && stations.map(station => (
             <div className="widget-ut" key={station.UICCode}>
                 <p className="stationHeader">
                     <img src={NsLogo} className="ns-logo" alt="NS Logo" />
@@ -58,16 +48,53 @@ const NSWidget = () => {
                 </div>
                 <StationWidget stationID={station.UICCode} />
             </div>
-        );
-    };
+        ))
+    ;
+
+    // const renderSingleStation = () => {
+    //     if (stations.length === 0) return <p>No stations available</p>;
+    //     const station = stations.find(station => station.namen.lang === 'Utrecht CS') || stations[1];
+
+    //     return (
+    //         <div className="widget-ut" key={station.UICCode}>
+    //             <p className="stationHeader">
+    //                 <img src={NsLogo} className="ns-logo" alt="NS Logo" />
+    //                 {station.namen.lang}
+    //             </p>
+    //             <div className="NSspacing bg-yellow-dark">
+    //                 <p>Tijd</p>
+    //                 <p>Bestemming</p>
+    //                 <p>Trein</p>
+    //                 <p className="platformHeader">Perron</p>
+    //             </div>
+    //             <StationWidget stationID={station.UICCode} />
+    //         </div>
+    //     );
+    // };
 
     return (
         <div className="stations widget bg-yellow-light">
             {error ? (
                 <p>{error}</p>
-            ) : (
-                renderSingleStation()
-            )}
+            ) :
+            <Slider>
+            {stations && stations.map(station => (
+                <div className="widget-ut" key={station.UICCode}>
+                    <p className="stationHeader">
+                        <img src={NsLogo} className="ns-logo" alt="NS Logo" />
+                        {station.namen.lang}
+                    </p>
+                    <div className="NSspacing bg-yellow-dark">
+                        <p>Tijd</p>
+                        <p>Bestemming</p>
+                        <p>Trein</p>
+                        <p className="platformHeader">Perron</p>
+                    </div>
+                    <StationWidget stationID={station.UICCode} />
+                </div>
+            ))}
+            </Slider> 
+            }
         </div>
     );
 };
--- src/Components/StationWidget.jsx
+++ src/Components/StationWidget.jsx
@@ -1,10 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { getTravelInfo } from '../API-requests/NS-API';
 import './NS.css';
+import { FaWalking } from "react-icons/fa";
 
 export default function StationWidget({ stationID }) {
     const url = `/api/departures?stationID=${stationID}&maxJourneys=40`
-    const { data, error, loading } = useQuery({ queryKey: ['departureInfo', stationID], queryFn: async () => await getTravelInfo(url), staleTime: 0, cacheTime: 60 * 1000, refetchInterval: 20 * 1000 },)
+    const { data, error, loading } = useQuery({ queryKey: ['departureInfo', stationID], queryFn: async () => await getTravelInfo(url), staleTime: 0, cacheTime: 60 * 1000, refetchInterval: 60 * 1000 },)
+
+    const DISTANCES = {
+        UC : 13,
+        UVR: 8
+    }
 
     let listOfTrains = data ? data.payload.departures.slice() : [];
     if (listOfTrains.length > 0) listOfTrains.pop();
@@ -20,27 +26,36 @@ export default function StationWidget({ stationID }) {
             const date = new Date(departureDateTime)
             const departureTime = date.toLocaleTimeString();
 
-            const newData = {...departure, departureTime: departureTime}
+            const newData = { ...departure, departureTime: departureTime }
             return newData
         }).filter(departure => departure.departureTime > newTime)
 
         return futureDepartures
     }
 
-    console.log()
-    const futureTrains = futureDepartures(listOfTrains, 12)
+    let futureTrains;
+
+    if (stationID == 8400621){
+        futureTrains = futureDepartures(listOfTrains, DISTANCES.UC)
+    } else {
+        futureTrains = futureDepartures(listOfTrains, DISTANCES.UVR)
+    }
+
     return (
         <div>
-            {listOfTrains ? (
+            {(listOfTrains && futureTrains) ? (
                 futureTrains.map((departure, index) => (
-                    <div className={`utrechtCSTrainDataContainer ${index % 2 === 0 ? 'bg-yellow-light' : 'bg-yellow-dark'}`} style={index >= 10 ? { display: 'none' } : { display: "grid" }} key={departure.UICCode}>
+                    <div className={`utrechtCSTrainDataContainer ${index % 2 === 0 ? 'bg-yellow-light' : 'bg-yellow-dark'}`} style={index >= 8 ? { display: 'none' } : { display: "grid" }} key={departure.UICCode}>
                         <p className='time'>{departure.actualDateTime.split('T')[1].split(':').slice(0, 2).join(':')}</p>
                         <p className='destination'>{departure.direction}</p>
-                        {departure.routeStations.length > 0 ? (
-                            <p className='routeVia'>Via {departure.routeStations.map(routeStation => routeStation.mediumName).join(', ')}</p>
-                        ) : (
-                            <p className='routeVia'>No route available</p>
-                        )}
+                        {departure.routeStations.length > 0 ?
+                            <div className='routeVia'> 
+                                <p>Via {departure.routeStations.map(routeStation => routeStation.mediumName).join(', ')}</p>
+                                {departure.messages.length > 0 && departure.messages.map(message => <p>* {message.message}</p>)}
+                                
+                            </div> : (
+                                <p className='routeVia'>No route available</p>
+                            )}
                         <p className='platform'>{departure.actualTrack}</p>
                         <p className='train-product'>{departure.product.categoryCode}</p>
                     </div>
