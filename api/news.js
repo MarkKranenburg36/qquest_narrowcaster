3b29c5b6c052c01f5798daa71c640bb958bdbcd5
Vasilis <tomaras.vasi@outlook.com>
2024-08-13 10:01:26 +0200

    apikey in url meegeven

--- api/news.js
+++ api/news.js
@@ -4,18 +4,18 @@ const CURRENT_KEY = 'vbUGjupVjZydXbadmM4XM-RcxkEsazHVLWAxiNDcpN-FeN6M';
 
 export default function handler(req, res) {
   const proxy = createProxyMiddleware({
-    target: 'https://api.currentsapi.services/v1/latest-news',
+    target: `https://api.currentsapi.services/v1/latest-news?${CURRENT_KEY}`,
     changeOrigin: true,
     pathRewrite: {
       '^/api': '',
     },
 
-    onProxyReq: (proxyReq, req, res) => {
-      // Add your custom headers here
-      proxyReq.setHeader('XS-API-KEY', `${CURRENT_KEY}`);
-      proxyReq.setHeader( 'Cache-Control', 'no-cache');
-      // Add any other headers you need
-    },
+    // onProxyReq: (proxyReq, req, res) => {
+    //   // Add your custom headers here
+    //   // proxyReq.setHeader('apiKey', `${CURRENT_KEY}`);
+    //   // proxyReq.setHeader( 'Cache-Control', 'no-cache');
+    //   // Add any other headers you need
+    // },
 
   });
   return proxy(req, res);
