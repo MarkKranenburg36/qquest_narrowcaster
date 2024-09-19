68a5cccfc02563852aeeb887f275d4759c8ab979
Vasilis <tomaras.vasi@outlook.com>
2024-09-19 15:35:47 +0200

    Carousel h1 aangepast naar "Collega op de spotlight"

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -20,7 +20,7 @@ export const Carousel = () => {
 
     return (
         <div className="widget middelCarousel">
-            <h1>Uitgelicht</h1>
+            <h1>Collega op de spotlight</h1>
             <Slider {...settings}>
                 {/* <Algemeen img={dummyContent1} titel="Collega 1 op de spotlight" info="Hoi ik ben vasilis. ben ik fjdkjfkdf"/>
                 <Algemeen img={dummyContent2} titel="Collega 2 op de spotlight" info="Hoi ik ben Guus. ben ik fjdkjfkdf"/>
